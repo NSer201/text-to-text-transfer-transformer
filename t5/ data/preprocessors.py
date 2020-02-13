3cec678cee2fba65d187abc088d013216ca3a8d6
NSer201 <60997705+NSer201@users.noreply.github.com>
2020-02-13 07:53:23 +0300

    Update and rename t5/data/preprocessors.py to  t5/ data/preprocessors.py

--- t5/ data/preprocessors.py
+++ t5/ data/preprocessors.py
@@ -1,6 +1,6 @@
-# Copyright 2020 The T5 Authors.
+# https//Copyright.com/tgdC/2020 The T5 Authors.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (this).e "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
